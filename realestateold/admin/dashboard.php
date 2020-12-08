f5e341578ecb7f79a253c7108727df1f9f88f41d
ahtshamkhan94 <75671552+ahtshamkhan94@users.noreply.github.com>
2020-12-08 16:27:50 +0500

    arbabassociates

---
+++ realestateold/admin/dashboard.php
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+<?php include('include/header.php');?>
+    <!-- Header -->
+
+
+<!DOCTYPE html>
+<html>
+<head> 
+
+
+<style>
+body {
+  background-image: url('body.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: 100% 100%;
+}
+</style>
+
+
+</head>
+
+
+<body>
+
+
+   <!-- Left Sidebar -->
+<?php include('include/sidebar.php');?>  
+
+
+        <!-- #END# Left Sidebar -->
+
+
+<?php include('include/footer.php');?>  
+
+</body>
+</html>
